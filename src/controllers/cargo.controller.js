0b0a4567c6d1a4f446ea4ad851757e7c2f31dfeb
Alberto <alberto.cabello64@gmail.com>
2023-01-24 20:25:44 -0300

    backend

--- src/controllers/cargo.controller.js
+++ src/controllers/cargo.controller.js
@@ -2,17 +2,32 @@ import {pool} from '../db.js'
 
 
 export const getCargos = async(req, res) => {
-    const [rows] = await pool.query('SELECT * FROM cargo')
-    res.send(rows);   
+    try{
+        const [rows] = await pool.query('SELECT * FROM cargo')
+        res.send(rows);   
+       
+    }catch(error){
+        return res.status(500).json({
+            message:'Algo salió mal'
+        })
+    }
 }
 
 export const getCargo = async(reg, res) => {
-    const [rows] = await pool.query('SELECT * FROM cargo WHERE idCargo = ?', [reg.params.id])
-    if(rows.length <= 0) return res.status(404).json({
-        message: 'El carno no existe'
-    })
-    res.send(rows[0])
-}
+    try{
+        const [rows] = await pool.query('SELECT * FROM cargo WHERE idCargo = ?', [reg.params.id])
+        if(rows.length <= 0) return res.status(404).json({
+            message: 'El cargo no existe'
+        })
+        res.send(rows[0])
+    
+    }catch(error){
+        return res.status(500).json({
+          message : 'Algo salió mal'  
+        })
+    }
+    
+    }
 
 export const createCargo = (req, res) => res.send('Creando Cargo');
 export const updateCargo = (req, res) => res.send('Actulizando Cargo');
